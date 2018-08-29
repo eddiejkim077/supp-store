18f213d304a7409c9fdd799c531ec3e486af0848
eddiejkim077 <eddiejkim077@gmail.com>
2018-08-28 20:28:48 -0700

    Adding product display page

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import './ProductPage.css';
 
 const ProductPage = (props) => {
   return (
     <div className="ProductPage">
       <h1>{props.product.name}</h1>
-      <h1>{props.product.description}</h1>
+      <img src={props.product.image} />
+      <p>{props.product.description}</p>
+            
       <button onClick={() => props.handleAddItem(props.product._id)}> ADD TO CART </button>
     </div>
   )
