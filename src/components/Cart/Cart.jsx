40972a3d4d69ab01f66e692d761ca450ca73b12f
eddiejkim077 <eddiejkim077@gmail.com>
2018-09-03 20:49:53 -0700

    Adding checkout address form

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -12,26 +12,27 @@ const Cart = (props) => {
             handleRemoveQuantity={props.handleRemoveQuantity}
             handleAddItem={props.handleAddItem}
           />
-            <div className="CartTotal position-fixed">
-              <h4> Billing Address </h4>
-              <label for="fname"><i class="fa fa-user" /> Full Name</label>
-              <input type="text" id="fname" name="firstname" placeholder="Eddie J. Kim" />
-              <label for="adr"><i class="fa fa-address-card-o" /> Address</label>
-              <input type="text" id="adr" name="address" placeholder="542 W. 15th Street" />
-              <label for="city"><i class="fa fa-institution" /> City</label>
-              <input type="text" id="city" name="city" placeholder="Pasadena" />
-              <div className="StateAndZip">
-                  <label for="state">State</label>
-                  <input type="text" id="state" name="state" placeholder="CA" />
-                  <label for="zip">Zip</label>
-                  <input type="text" id="zip" name="zip" placeholder="91106" />
-              </div>
-              <h4>subtotal: ${parseFloat(total).toFixed(2)}</h4>
-              <h4 className="CartTotalTax">tax: 7.25%</h4>
-              <h2>total: ${parseFloat(total + (total * .0725)).toFixed(2)}</h2>
-          </div>
+          <div className="CartTotal position-fixed">
+            <h4>Shipping Address</h4>
+            <label for="fname"><i class="fa fa-user" /> Full Name</label>
+            <input type="text" id="fname" name="firstname" placeholder="Eddie J. Kim" />
+            <label for="adr"><i class="fa fa-address-card-o" /> Address</label>
+            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street" />
+            <label for="city"><i class="fa fa-institution" /> City</label>
+            <input type="text" id="city" name="city" placeholder="Pasadena" />
+            <div className="StateAndZip">
+              <label for="state">State</label>
+              <input type="text" id="state" name="state" placeholder="CA" />
+              <label for="zip">Zip</label>
+              <input type="text" id="zip" name="zip" placeholder="91106" />
+              <button className="btn btn-primary">Checkout</button>
+            </div>
+            <h4>subtotal: ${parseFloat(total).toFixed(2)}</h4>
+            <h4 className="CartTotalTax">tax: 7.25%</h4>
+            <h2>total: ${parseFloat(total + (total * .0725)).toFixed(2)}</h2>
         </div>
-      </section>
+      </div>
+    </section>
   )
 }
 
